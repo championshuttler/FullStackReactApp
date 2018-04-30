0c45580be90de6b6c582c2c92d634c73b683bdd8
championshuttler <shivams2799@gmail.com>
2018-04-30 14:26:39 +0530

    iinitializing repo

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express'); // CommonJS module
+
+// import express from 'express'; // for react side code
+const app = express();
+
+app.get('/', (req, res)=>{  // whole function is route handler
+  res.send({ hi : 'there'});
+});
+
+const PORT = process.env.PORT || 5000; // port from heroku or something else
+
+app.listen(PORT); // localhost:5000 port to listen the app
