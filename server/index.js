5b39d009f3c957796873eff40467b86eeca7194e
championshuttler <shivams2799@gmail.com>
2018-05-01 00:18:21 +0530

    adding passportjs dependencies

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 const express = require('express'); // CommonJS module
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 // import express from 'express'; // for react side code
 const app = express();
 
-app.get('/', (req, res)=>{  // whole function is route handler
-  res.send({ bye : 'buddy'});
-}); 
+passport.use(new GoogleStrategy());
+
+// app.get('/', (req, res)=>{  // whole function is route handler
+//   res.send({ bye : 'buddy'});
+// }); 
+
 
 const PORT = process.env.PORT || 5000; // port from heroku or something else
 
