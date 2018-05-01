652350a694ce3450a18d7db66eaaa642c46138a8
championshuttler <shivams2799@gmail.com>
2018-05-01 18:09:48 +0530

    making code prettier

--- server/index.js
+++ server/index.js
@@ -1,35 +1,12 @@
 const express = require('express'); // CommonJS module
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
+require('./services/passport');
+
 
 // import express from 'express'; // for react side code
 const app = express();
 
-passport.use(
-  new GoogleStrategy(    // google strategy
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
-    },
-    (accessToken, refreshToken, profile,done) => {
-      console.log('access token: ',accessToken);
-      console.log('refresh token: ',refreshToken);
-      console.log('profile: ',profile);
-    }
-  )
-);
+require('./routes/authRoutes')(app);
 
-app.get(
-  '/auth/google',   // authentication from passport
-  passport.authenticate('google',{
-    scope: ['profile', 'email']
-  })
-);
-
-app.get('/auth/google/callback', 
-    passport.authenticate('google'));
 
 
 // app.get('/', (req, res)=>{  // whole function is route handler
