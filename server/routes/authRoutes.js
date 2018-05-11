87fae61c7f75e135f3a172a58d7cde609d0af537
championshuttler <shivams2799@gmail.com>
2018-05-12 00:48:13 +0530

    feature to logout

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -12,6 +12,12 @@ module.exports = app => {
   app.get('/auth/google/callback',
     passport.authenticate('google'));
 
+    //router for logout 
+  app.get('/api/logout', (req, res) => { 
+    req.logout();
+    res.send(req.user);
+  });
+  // router for getting id from mongodb database
   app.get('/api/current_user', (req, res) => {
     res.send(req.user);
   })
