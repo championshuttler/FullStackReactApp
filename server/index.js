ea7ba5d3ebe01a60cc4282cbcb667af0ca67c48b
championshuttler <shivams2799@gmail.com>
2018-05-11 23:07:08 +0530

    enabling cookies #40

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express'); // CommonJS module
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
 require('./models/user');         // changed the sequence due to error "Schema hasn't been registered for model "users"."
 require('./services/passport');
@@ -8,11 +10,20 @@ require('./services/passport');
 // import express from 'express'; // for react side code
 
 mongoose.connect(keys.mongoURI);   // connect to mongoDB with mongoose
+
 const app = express();
 
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 require('./routes/authRoutes')(app);
-
-
 
 // app.get('/', (req, res)=>{  // whole function is route handler
 //   res.send({ bye : 'buddy'});
